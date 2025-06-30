8fe483b3231e97730ef6d7fab9f11e124a5b0cd0
Ayu Fitria <fitrimut0711@gmail.com>
2025-06-30 17:30:53 +0700

    Selesaikan merge conflict routes/web.php

--- config/livewire.php
+++ config/livewire.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'layout' => 'livewire.backend.backendlayout',
+    'layout' => 'layouts.app',
 
     /*
     |---------------------------------------------------------------------------
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,17 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin12@gmail.com',
+            'password' => bcrypt('rahasia'),
+            'role' => 'admin'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Kasir',
+            'email' => 'kasir56@gmail.com',
+            'password' => bcrypt('kasirSR'),
+            'role' => 'kasir'
         ]);
     }
 }
